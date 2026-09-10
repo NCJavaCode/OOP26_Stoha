4b52fb0dc958423a88afa0be93b3485783a15af1
Sanya <oleksandrstoga@gmail.com>
2026-09-10 12:11:59 +0300

    Додав 3 класи

---
+++ src/Storage.java
@@ -0,0 +1,27 @@
+public class Storage {
+    private String address;
+    private int capacity;
+
+    public Storage(String address, int capacity) {
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        if (capacity > 0) {
+            this.capacity = capacity;
+        }
+    }
+}
