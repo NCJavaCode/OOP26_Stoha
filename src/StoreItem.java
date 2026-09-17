b751458a0b7853ed81da3775958ba658a5bfd047
Sanya <oleksandrstoga@gmail.com>
2026-09-17 11:45:48 +0300

    АБСТРАКТНИЙ КЛАС

---
+++ src/StoreItem.java
@@ -0,0 +1,18 @@
+public abstract class StoreItem {
+
+    protected String name;
+
+    public StoreItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void showInfo();
+}
