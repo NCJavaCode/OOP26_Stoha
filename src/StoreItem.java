d46479d57dcd4fc56243d844cccd2b8ae7f66edb
!phratry <oleksandrstoga@gmail.com>
2026-09-17 12:06:02 +0300

    Update StoreItem.java
    Updated StoreItem class to include price attribute and related methods.

--- src/StoreItem.java
+++ src/StoreItem.java
@@ -1,17 +1,29 @@
 public abstract class StoreItem {
 
     protected String name;
+    protected double price;
 
-    public StoreItem(String name) {
+    public StoreItem(String name, double price) {
         this.name = name;
+        this.price = price;
     }
 
     public String getName() {
         return name;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        }
     }
 
     public abstract void showInfo();
